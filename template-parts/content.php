1c99775807b6db6200bf3716b3244d25c793a3a4
Joseph Maxim <josephmdagunan@gmail.com>
2018-09-14 16:46:13 -0400

    change permission

--- template-parts/content.php
+++ template-parts/content.php
@@ -31,27 +31,27 @@
 
 	<!-- <?php // josephmaxim_post_thumbnail(); ?> -->
 
-	<!-- <div class="entry-content">
+	<div class="entry-content">
 		<?php
-		// the_content( sprintf(
-		// 	wp_kses(
-		// 		/* translators: %s: Name of current post. Only visible to screen readers */
-		// 		__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'josephmaxim' ),
-		// 		array(
-		// 			'span' => array(
-		// 				'class' => array(),
-		// 			),
-		// 		)
-		// 	),
-		// 	get_the_title()
-		// ) );
+		the_content( sprintf(
+			wp_kses(
+				/* translators: %s: Name of current post. Only visible to screen readers */
+				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'josephmaxim' ),
+				array(
+					'span' => array(
+						'class' => array(),
+					),
+				)
+			),
+			get_the_title()
+		) );
 
-		// wp_link_pages( array(
-		// 	'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'josephmaxim' ),
-		// 	'after'  => '</div>',
-		// ) );
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'josephmaxim' ),
+			'after'  => '</div>',
+		) );
 		?>
-	</div> -->
+	</div>
 	<!-- .entry-content -->
 
 	<footer class="entry-footer">
