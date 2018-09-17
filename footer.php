79bd3ce5edea1a914e1c05e5d121861b304f2e1f
Joseph Maxim <josephmdagunan@gmail.com>
2018-09-16 23:58:23 -0400

    edited footer

--- footer.php
+++ footer.php
@@ -18,10 +18,10 @@
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'josephmaxim' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'josephmaxim' ), 'WordPress' );
+				// printf( esc_html__( 'Proudly powered by %s', 'josephmaxim' ), 'WordPress' );
 				?>
 			</a>
-			<span class="sep"> | </span>
+			<!-- <span class="sep"> | </span> -->
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'josephmaxim' ), 'josephmaxim', '<a href="https://josephmaxim.io">Joseph Maxim</a>' );
